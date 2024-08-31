ab69c2422b7eb0fc23cc52abe9bf73a50985ed5a
juan-gasero <159394842+juan-gasero@users.noreply.github.com>
2024-08-30 18:42:53 -0700

    Juan Fernando Gastelum Quintero 30/08/2024

---
+++ BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial1/Practica1.php
@@ -0,0 +1,16 @@
+<?php
+$Nombre ="Juan";
+$Numerito=4;
+$Numerote="60";
+$total= $Numerito + $Numerote;
+
+echo "total=" . $total;
+
+echo "
+<h1>HOLA MUNDO MUNDIAL EXCEPTO NORCOREA</h1>";
+for($i=0; $i<=10; $i++){
+    echo $i . "<br>";
+}
+echo "<br>";
+ echo "<center style='font-family: Impact; font-size:100;'>Gastélum Quintero Juan Fernando 5AVP</center>";
+?>
