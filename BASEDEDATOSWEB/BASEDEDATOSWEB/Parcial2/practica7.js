96f7a0f5f4a6f621b291bdbf16eb8cfbb7560f4c
juan-gasero <159394842+juan-gasero@users.noreply.github.com>
2024-09-27 18:19:36 -0700

    Gastélum Quintero Juan Fernando 27/09/2024
    TIRIRIRIIRIRTIRIRIRIRIRIRIR

---
+++ BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial2/practica7.js
@@ -0,0 +1,33 @@
+function validar(posicion) {
+    var cuadro= document.getElementById("cuadro"+posicion);
+ if (cuadro.value!="x"&& cuadro.value!= "o") {
+    console.log(cuadro.value+"-- valor no aceptado");
+    cuadro.value='';
+    return;
+ }else{
+    console.log("prueba");
+    var cuadro1 = document.getElementById("cuadro1").value;
+    var cuadro2 = document.getElementById("cuadro2").value;
+    var cuadro3 = document.getElementById("cuadro3").value;
+    var cuadro4 = document.getElementById("cuadro4").value;
+    var cuadro5 = document.getElementById("cuadro5").value;
+    var cuadro6 = document.getElementById("cuadro6").value;
+    var cuadro7 = document.getElementById("cuadro7").value;
+    var cuadro8 = document.getElementById("cuadro8").value;
+    var cuadro9 = document.getElementById("cuadro9").value;
+
+    switch(posicion){
+        case 1:
+         if (cuadro1 == cuadro2 && cuadro1 == cuadro3 )  {
+            alert("EL GANADOR ES--->"+ cuadro1 ); 
+           }else if(cuadro1 == cuadro5 && cuadro1 == cuadro9 ){ 
+            alert("EL GANADOR ES--->"+ cuadro1 ); 
+
+           }else if(cuadro1 == cuadro4 && cuadro1 == cuadro7 ){ 
+            alert("EL GANADOR ES--->"+ cuadro1 ); 
+
+           }
+        break;
+    }
+ }
+}
