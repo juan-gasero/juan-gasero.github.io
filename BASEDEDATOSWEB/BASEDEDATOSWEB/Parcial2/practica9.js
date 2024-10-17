13ab8fd022b0718cbfeeac5dbb7a63ba726abc30
juan-gasero <159394842+juan-gasero@users.noreply.github.com>
2024-10-16 18:28:36 -0700

    Gastélum Quintero Juan Fernando 16/10/2024
    js ya sirvio gracias a luis carlos redentor

--- BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial2/practica9.js
+++ BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial2/practica9.js
@@ -1,24 +1,61 @@
 $(document).ready(function(){
-    let cartas = new Array(54).fill(false);
-    $("#dar_carta").click(function(){
-        var b = true;
-        var cont = 0;
+    let cartas =new Array(54).fill(false);
 
+    //funcion click del boton dar carta :v
+    $("#dar_carta").click(function(){
+
+        var b = true;
+
+        var cont = 0;
         while(b){
-            var num= Math.floor(Math.random()*54) + 1;
-            console.log("Numero de carta= "+ num);
+            var num = Math.floor(Math.random()*54) + 1;
+            console.log("Numero de carta="+num);
             if(cartas[num-1] == true){
                 continue;
             }else{
-                $("#carta_activada").html("<img src='loteria/" + num + ".jpg'>");
+                $("#carta_activada").html("<img src='loteria/"+num+".jpg'>");
                 cartas[num-1] = true;
-                b=false;
+                b=false; 
                 cont++;
             }
             if(cont == 54){
-                console.log("ya se dieron todas las cartas");
+                console.log("YA SE ACABARON LAS CARTAS");
                 b=false;
             }
         }
     });
+
+    cargarCartas();
+
+    function cargarCartas(){
+        var carta1 = 0;
+        var carta2 = 0;
+        let asignadas = new Array(54).fill(false);
+
+        while(carta1 < 16){
+            var num =Math.floor(Math.random() * 54)+1;
+            if(asignadas[num-1]== false){
+                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true; 
+                carta1++;
+            }else{
+                continue;
+            }
+        }
+
+        asignadas = new Array(54).fill(false);
+
+        while(carta2 < 16){
+            var num =Math.floor(Math.random() * 54)+1;
+            if(asignadas[num-1]== false){
+                $("#carta2").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true; 
+                carta2++;
+            }else{
+                continue;
+            }
+        }
+        
+    }
+
 });
