8b0a16564621537a2394209e2b8d2df5f285b22b
juan-gasero <159394842+juan-gasero@users.noreply.github.com>
2024-11-14 17:47:35 -0700

    Gastélum Quintero Juan Fernando 14/11/2024

--- BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial2/practica9.js
+++ BASEDEDATOSWEB/BASEDEDATOSWEB/Parcial2/practica9.js
@@ -1,61 +1,57 @@
-$(document).ready(function(){
-    let cartas =new Array(54).fill(false);
+$(document).ready(function (){
+    let cartas = new Array(54).fill(false);
+    var cont = 0;
+    var n_carta = 0;
+   //Funcion click del boton dar carta
+   $("#dar_carta").click(function(){
+       var b = true;
+       while(b){
+           var num = Math.floor(Math.random()*54) + 1;
+           console.log("Numero de carta= "+num);
+           if(cartas[num-1] == true){
+               continue;
+           } else {
+               $("#carta_activa").html("<img width='120px' src='loteria/"+num+".jpg'>");
+               cartas[num-1] = true;
+               b=false;
+               cont++;
+           }
+           if(cont == 54){
+               console.log("ya se dieron todas las cartas");
+               b=false;
+           }
+       }
+   });
 
-    //funcion click del boton dar carta :v
-    $("#dar_carta").click(function(){
+   cargarCartas();
 
-        var b = true;
+   function cargarCartas(){
+       var carta1 = 0;
+       var carta2 = 0;
+       let asignadas = new Array(54).fill(false);
 
-        var cont = 0;
-        while(b){
-            var num = Math.floor(Math.random()*54) + 1;
-            console.log("Numero de carta="+num);
-            if(cartas[num-1] == true){
-                continue;
-            }else{
-                $("#carta_activada").html("<img src='loteria/"+num+".jpg'>");
-                cartas[num-1] = true;
-                b=false; 
-                cont++;
-            }
-            if(cont == 54){
-                console.log("YA SE ACABARON LAS CARTAS");
-                b=false;
-            }
-        }
-    });
+       while(carta1 < 16){
+           var num = Math.floor(Math.random()*54) + 1;
+           if(asignadas[num-1] == false){
+               $("#carta1").append("<div class='col-sm-3 border'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+               asignadas[num-1] = true;
+               carta1++;
+           } else {
+               continue;
+           }
+       }
 
-    cargarCartas();
+       asignadas = new Array(54).fill(false);
 
-    function cargarCartas(){
-        var carta1 = 0;
-        var carta2 = 0;
-        let asignadas = new Array(54).fill(false);
-
-        while(carta1 < 16){
-            var num =Math.floor(Math.random() * 54)+1;
-            if(asignadas[num-1]== false){
-                $("#carta1").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
-                asignadas[num-1] = true; 
-                carta1++;
-            }else{
-                continue;
-            }
-        }
-
-        asignadas = new Array(54).fill(false);
-
-        while(carta2 < 16){
-            var num =Math.floor(Math.random() * 54)+1;
-            if(asignadas[num-1]== false){
-                $("#carta2").append("<div class='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
-                asignadas[num-1] = true; 
-                carta2++;
-            }else{
-                continue;
-            }
-        }
-        
-    }
-
+       while(carta2 < 16){
+           var num = Math.floor(Math.random()*54) + 1;
+           if(asignadas[num-1] == false){
+               $("#carta2").append("<div class='col-sm-3 border'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+               asignadas[num-1] = true;
+               carta2++;
+           } else {
+               continue;
+           }
+       }
+   }
 });
